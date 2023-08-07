b9791d625dde03f15515f86ec577802ea1f37ae5
Abhilasha <abhilasha15597@gmail.com>
2023-08-07 22:36:18 +0530

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-	params: { hl: "en", gl: "IN" },
+	params: { hl: "hi", gl: "IN" },
 	headers: {
 		"X-RapidAPI-Key":
 			process.env.REACT_APP_YOUTUBE_API_KEY ||
