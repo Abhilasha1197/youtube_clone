a593eaff8bc1d229bf45ac44c9ff3d01078e2c79
Abhilasha <abhilasha15597@gmail.com>
2023-08-07 18:09:05 +0530

    first-commit

---
+++ src/utils/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+	params: { hl: "en", gl: "US" },
+	headers: {
+		"X-RapidAPI-Key":
+			process.env.REACT_APP_YOUTUBE_API_KEY ||
+			"76c6cb2d7bmsh220ac7cc984ec89p14808bjsn3101f8bc156a",
+		"X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+	},
+};
+
+export const fetchDataFromApi = async (url) => {
+	const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+	return data;
+};
